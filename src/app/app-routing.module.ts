3468b56e1b4bb4363d0d67206729fcb4f98262ad
ramya <rallada@v2soft.com>
2020-01-23 17:36:05 +0530

    app changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateEmployeeComponent } from './employee/create-employee/create-employee.component';
+import { ListEmployeesComponent } from './employee/list-employees/list-employees.component';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { RegisterLoginComponent } from './register-login/register-login.component';
+import { TicketsComponent } from './tickets/tickets.component';
+import { ProductComponent } from './product/product.component';
 
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+   { path: 'list', component: ListEmployeesComponent },
+  { path: 'create', component: CreateEmployeeComponent },
+  { path: 'user', component: CreateUserComponent },
+ { path: 'login', component: RegisterLoginComponent }, 
+ { path: 'tickets', component: TicketsComponent },
+ { path: 'product', component: ProductComponent }, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateEmployeeComponent } from './employee/create-employee/create-employee.component';
+import { ListEmployeesComponent } from './employee/list-employees/list-employees.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { FormsModule } from '@angular/forms';
+import { RegisterLoginComponent } from './register-login/register-login.component';
+import { TicketsComponent } from './tickets/tickets.component';
+import { ProductComponent } from './product/product.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEmployeeComponent,
+    ListEmployeesComponent,
+    CreateUserComponent,
+    RegisterLoginComponent,
+    TicketsComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
